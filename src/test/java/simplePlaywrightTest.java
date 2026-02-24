2a500acf61160d8f246752fe50d3c4df75a9159e
Nikita Mekhanikov <nikitamekhanikov@coherentsolutions.com>
2026-02-24 15:47:41 +0300

    Refactor first tests

--- src/test/java/simplePlaywrightTest.java
+++ src/test/java/simplePlaywrightTest.java
@@ -1,23 +1,30 @@
 import com.microsoft.playwright.Browser;
 import com.microsoft.playwright.Page;
 import com.microsoft.playwright.Playwright;
+import com.microsoft.playwright.junit.UsePlaywright;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+@UsePlaywright
 public class simplePlaywrightTest {
 
-
     @Test
-    void titleAppearsTest() {
-        Playwright playwright = Playwright.create();
-        Browser browser = playwright.chromium().launch();
-        Page titlePage = browser.newPage();
-
+    void titleAppearsTest(Page titlePage) {
         titlePage.navigate("https://practicesoftwaretesting.com");
         String titleName=titlePage.title();
 
         Assertions.assertTrue(titleName.contains("Practice Software Testing"));
-        browser.close();
-        playwright.close();
+    }
+
+    @Test
+    void searchFieldTest(Page titlePage){
+        titlePage.navigate("https://practicesoftwaretesting.com");
+        titlePage.locator("[Placeholder=Search]").fill("Pliers");
+        titlePage.locator("button:has-text('Search')").click();
+
+        int matchingSearchResult =titlePage.locator(".card").count();
+        Assertions.assertTrue(matchingSearchResult>0);
     }
 }
